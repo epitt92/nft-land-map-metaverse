5bd86f24ace7e258fe4a8dc015afe3e44ecc4507
memokey <skytopground@gmail.com>
2022-04-18 10:45:46 -0300

    init

--- server/routes/landnfts.js
+++ server/routes/landnfts.js
@@ -9,7 +9,7 @@ var mysql = require('mysql')
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "",
+  password: "topsecret",
   port:"3306",
   database:"metatruffy"
 });
